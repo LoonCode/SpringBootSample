ba373b8758e567924f27b8dfe7149511a4815733
Loon <ppgl1988@gmail.com>
2016-05-18 23:43:45 +0800

    init

--- src/main/java/com/sample/config/WebApp.java
+++ src/main/java/com/sample/config/WebApp.java
@@ -2,11 +2,13 @@ package com.sample.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by loon on 16/5/18.
  */
 @SpringBootApplication
+@ComponentScan(basePackages = "com.sample.controller")
 public class WebApp {
 
     public static void main(String[] args) {
